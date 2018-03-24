e7c734ebb539011dd06fe2d8fad9845336818368
LoveLoveCoCo <32346476+LoveLoveCoCo@users.noreply.github.com>
2018-03-25 00:45:54 +0800

    Add files via upload

---
+++ cocoread.py
@@ -0,0 +1,179 @@
+# -*- coding: utf-8 -*-
+from linepy import *
+from datetime import datetime
+import codecs, json, time, timeit
+#cl = LINE('LINE帳號','LINE密碼')
+cl = LINE("ErTzVdUbWP5bw6TTTTse.z+eOpBplMO1PsYShrS6LBG.Qp7e2sebX0roCCNONVUHKLChMKQi+oaIGmZXIUbhzOE=")
+oepoll = OEPoll(cl)
+settingsOpen = codecs.open("temp.json","r","utf-8")
+settings = json.load(settingsOpen)
+clMID = cl.profile.mid
+admin=['u28d781fa3ba9783fd5144390352b0c24',clMID]
+wait2 = {
+    'readPoint':{},
+    'readMember':{},
+    'setTime':{},
+    'ROM':{}
+}
+setTime = {}
+setTime = wait2['setTime']
+msg_dict = {}
+bl = [""]
+def backupData():
+    try:
+        backup = settings
+        f = codecs.open('temp.json','w','utf-8')
+        json.dump(backup, f, sort_keys=True, indent=4, ensure_ascii=False)
+        return True
+    except Exception as error:
+        logError(error)
+        return False
+def logError(text):
+    cl.log("[ ERROR ] " + str(text))
+    time_ = datetime.now()
+    with open("errorLog.txt","a") as error:
+        error.write("\n[%s] %s" % (str(time), text))
+def helpmessage():
+    helpMessage = """
+╔═══════════
+╠♥ ✿ CoCo指令表 ✿ ♥
+╠➥ 「Speed」查看機器速度
+╠➥ 「Set」查看設定
+╠➥ 「Reread On/Off」查看收回 打開/關閉
+╠➥ 「Setread」設定已讀點
+╠➥ 「Lookread」查詢已讀點
+╠➥ 「Tagall」標註全部人
+╚═〘 Credits By: ©CoCo™  〙
+"""
+    return helpMessage
+def lineBot(op):
+    try:
+        if op.type == 0:
+            return
+        if op.type == 26 or op.type == 25:
+            msg = op.message
+            text = msg.text
+            receiver = msg.to
+            sender = msg._from
+            if msg.toType == 0:
+                if sender != cl.profile.mid:
+                    to = sender
+                else:
+                    to = receiver
+            else:
+                to = receiver
+            if msg.contentType == 0:
+                if text is None:
+                    return
+            if sender in admin:
+                if text.lower() == 'help':
+                    helpMessage = helpmessage()
+                    cl.sendMessage(to, str(helpMessage))
+                    cl.sendContact(to, "u28d781fa3ba9783fd5144390352b0c24")
+                elif msg.text in ["SR","Setread"]:
+                    cl.sendMessage(msg.to, "設置已讀點")
+                    try:
+                        del wait2['readPoint'][msg.to]
+                        del wait2['readMember'][msg.to]
+                    except:
+                        pass
+                    now2 = datetime.now()
+                    wait2['readPoint'][msg.to] = msg.id
+                    wait2['readMember'][msg.to] = ""
+                    wait2['setTime'][msg.to] = datetime.strftime(now2,"%H:%M")
+                    wait2['ROM'][msg.to] = {}
+                elif msg.text in ["LR","Lookread"]:
+                    if msg.to in wait2['readPoint']:
+                        if wait2["ROM"][msg.to].items() == []:
+                            chiya = ""
+                        else:
+                            chiya = ""
+                            for rom in wait2["ROM"][msg.to].items():
+                                chiya += rom[1] + "\n"
+                        cl.sendMessage(msg.to, "||已讀的人||%s\n[%s]" % (wait2['readMember'][msg.to],setTime[msg.to]))
+                    else:
+                        cl.sendMessage(msg.to, "請輸入SR設置已讀點")
+                elif text.lower() == 'speed':
+                    time0 = timeit.timeit('"-".join(str(n) for n in range(100))', number=10000)
+                    str1 = str(time0)
+                    start = time.time()
+                    cl.sendMessage(to,'處理速度\n' + str1 + '秒')
+                    elapsed_time = time.time() - start
+                    cl.sendMessage(to,'指令反應\n' + format(str(elapsed_time)) + '秒')
+                elif text.lower() == 'set':
+                    ret_ = "╔══[ 設定 ]"
+                    if settings["reread"] == True: ret_ += "\n╠ 查詢收回開啟 ✅"
+                    else: ret_ += "\n╠ 查詢收回關閉 ❌"
+                    ret_ += "\n╚══[ 設定 ]"
+                    cl.sendMessage(to, str(ret_))
+                elif text.lower() == 'reread on':
+                    settings["reread"] = True
+                    cl.sendMessage(to, "查詢收回開啟")
+                elif text.lower() == 'reread off':
+                    settings["reread"] = False
+                    cl.sendMessage(to, "查詢收回關閉")
+                elif text.lower() == 'tagall':
+                    group = cl.getGroup(msg.to)
+                    nama = [contact.mid for contact in group.members]
+                    k = len(nama)//100
+                    for a in range(k+1):
+                        txt = u''
+                        s=0
+                        b=[]
+                        for i in group.members[a*100 : (a+1)*100]:
+                            b.append({"S":str(s), "E" :str(s+6), "M":i.mid})
+                            s += 7
+                            txt += u'@Alin \n'
+                        cl.sendMessage(to, text=txt, contentMetadata={u'MENTION': json.dumps({'MENTIONEES':b})}, contentType=0)
+                        cl.sendMessage(to, "總共 {} 個成員".format(str(len(nama))))
+        if op.type == 26:
+            try:
+                msg = op.message
+                if settings["reread"] == True:
+                    if msg.toType == 0:
+                        cl.log("[%s]"%(msg._from)+msg.text)
+                    else:
+                        cl.log("[%s]"%(msg.to)+msg.text)
+                    if msg.contentType == 0:
+                        msg_dict[msg.id] = {"text":msg.text,"from":msg._from,"createdTime":msg.createdTime}
+                else:
+                    pass
+            except Exception as e:
+                print(e)
+        if op.type == 65:
+            try:
+                at = op.param1
+                msg.id = op.param2
+                if settings["reread"] == True:
+                    if msg.id in msg_dict:
+                        if msg_dict[msg.id]["from"] not in bl:
+                            cl.sendMessage(at,"[收回訊息者]\n%s\n[訊息內容]\n%s"%(cl.getContact(msg_dict[msg.id]["from"]).displayName,msg_dict[msg.id]["text"]))
+                        del msg_dict[msg.id]
+                else:
+                    pass
+            except Exception as e:
+                print(e)
+        if op.type == 55:
+            try:
+                if op.param1 in wait2['readPoint']:
+                    Name = cl.getContact(op.param2).displayName
+                    if Name in wait2['readMember'][op.param1]:
+                        pass
+                    else:
+                        wait2['readMember'][op.param1] += "\n[•]" + Name
+                        wait2['ROM'][op.param1][op.param2] = "[•]" + Name
+                else:
+                    pass
+            except:
+                pass
+    except Exception as error:
+        logError(error)
+while True:
+    try:
+        ops = oepoll.singleTrace(count=50)
+        if ops is not None:
+            for op in ops:
+                lineBot(op)
+                oepoll.setRevision(op.revision)
+    except Exception as e:
+        logError(e)
