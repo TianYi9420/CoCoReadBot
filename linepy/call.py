68f14fb7bce00b81ddf2d849fb1a87388a324a29
LoveLoveCoCo <32346476+LoveLoveCoCo@users.noreply.github.com>
2018-06-19 10:01:56 +0800

    Update call.py

--- linepy/call.py
+++ linepy/call.py
@@ -14,19 +14,3 @@ class Call(object):
 
     def __init__(self):
         self.isLogin = True
-        
-    @loggedIn
-    def acquireCallRoute(self, to):
-        return self.call.acquireCallRoute(to)
-        
-    @loggedIn
-    def acquireGroupCallRoute(self, groupId, mediaType=MediaType.AUDIO):
-        return self.call.acquireGroupCallRoute(groupId, mediaType)
-
-    @loggedIn
-    def getGroupCall(self, ChatMid):
-        return self.call.getGroupCall(ChatMid)
-        
-    @loggedIn
-    def inviteIntoGroupCall(self, chatId, contactIds=[], mediaType=MediaType.AUDIO):
-        return self.call.inviteIntoGroupCall(chatId, contactIds, mediaType)
